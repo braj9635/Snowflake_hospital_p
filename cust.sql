faf5d1e356be600b72a6667ba10987242ae9dcff
braj9635 <basavamanvi2@gmail.com>
2026-02-13 21:44:35 +0000

    int

---
+++ cust.sql
@@ -0,0 +1,14 @@
+
+============================================================================================================
+
+   create OR REPLACE storage integration STORAGE_S3_INTEGRATION
+    type = external_stage
+    storage_provider = s3
+    storage_aws_role_arn = 'arn:aws:iam::278891233718:role/iam_snoflake_role'
+    enabled = true
+    storage_allowed_locations = ( 's3://new-hospital-db' )
+    -- storage_blocked_locations = ( 's3://<location1>', 's3://<location2>' )
+    -- comment = '<comment>'
+    ;
+
+       DESCRIBE storage integration STORAGE_S3_INTEGRATION;
