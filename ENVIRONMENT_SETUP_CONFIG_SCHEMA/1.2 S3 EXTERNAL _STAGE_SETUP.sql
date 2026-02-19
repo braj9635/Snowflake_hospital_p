badd5c5beced607ffc445d4b391ea7dce16271ea
braj9635 <basavamanvi2@gmail.com>
2026-02-19 08:25:02 +0000

    integration with s3 bucket

--- ENVIRONMENT_SETUP_CONFIG_SCHEMA/1.2 S3 EXTERNAL _STAGE_SETUP.sql
+++ ENVIRONMENT_SETUP_CONFIG_SCHEMA/1.2 S3 EXTERNAL _STAGE_SETUP.sql
@@ -1,22 +1,36 @@
 -- ============================================================
--- 1.2 S3 EXTERNAL STAGE SETUP
+-- 1.2 S3 EXTERNAL STORAGE INTEGRATION SETUP
 -- ============================================================
 
 CREATE OR REPLACE STORAGE INTEGRATION S3_HOSPITAL_INT
     TYPE = EXTERNAL_STAGE
     STORAGE_PROVIDER = 'S3'
     ENABLED = TRUE
-    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::role/snowflake-hospital-role'
-    STORAGE_ALLOWED_LOCATIONS = ('s3://hospital-data-bucket/');
+    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::278891233718:role/IAM_SNOW_ROLE'
+    STORAGE_ALLOWED_LOCATIONS = ('s3://new-hospital-db/')
+    COMMENT = 'AWS S3 POLICY AND USER';
 
+    DESC STORAGE INTEGRATION S3_HOSPITAL_INT;
+
+-- ============================================================
+--  S3 EXTERNAL STAGE 
+-- ============================================================
+    
 CREATE OR REPLACE STAGE HOSPITAL_ANALYTICS.RAW.STG_S3_HOSPITAL
     STORAGE_INTEGRATION = S3_HOSPITAL_INT
-    URL = 's3://hospital-data-bucket/'
+    URL = 's3://new-hospital-db/'
     FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' 
                    SKIP_HEADER = 1 NULL_IF = ('NULL', 'null', '', 'N/A')
                    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE);
 
--- File Formats
+LIST@RAW.STG_S3_HOSPITAL;  -- Fetch All Files in s3 bucket
+
+-- ============================================================
+--  File Formats
+-- ============================================================
+
+-- CSV
+
 CREATE OR REPLACE FILE FORMAT HOSPITAL_ANALYTICS.RAW.FF_CSV_HOSPITAL
     TYPE = 'CSV'
     FIELD_OPTIONALLY_ENCLOSED_BY = '"'
@@ -27,11 +41,15 @@ CREATE OR REPLACE FILE FORMAT HOSPITAL_ANALYTICS.RAW.FF_CSV_HOSPITAL
     TRIM_SPACE = TRUE
     DATE_FORMAT = 'YYYY-MM-DD'
     TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS';
-
+    
+--JSON
+    
 CREATE OR REPLACE FILE FORMAT HOSPITAL_ANALYTICS.RAW.FF_JSON_HOSPITAL
     TYPE = 'JSON'
     STRIP_OUTER_ARRAY = TRUE
     STRIP_NULL_VALUES = FALSE;
+
+--Parquet
 
 CREATE OR REPLACE FILE FORMAT HOSPITAL_ANALYTICS.RAW.FF_PARQUET_HOSPITAL
     TYPE = 'PARQUET'
